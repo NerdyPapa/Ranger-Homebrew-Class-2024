059374b11ccd26d9f34a023a7a7c500691f89e78
NerdyPapa <typar91@gmail.com>
2025-10-05 21:19:19 -0500

    Update spells.js

--- js/spells.js
+++ js/spells.js
@@ -21,13 +21,11 @@ const LEVEL1_SPELLS = {
   sorcerer: ["Burning Hands", "Charm Person", "Chromatic Orb", "Color Spray", "Comprehend Languages", "Detect Magic", "Disguise Self", "Expeditious Retreat", "False Life", "Feather Fall", "Fog Cloud", "Jump", "Mage Armor", "Magic Missile", "Ray of Sickness", "Shield", "Silent Image", "Sleep", "Thunderwave", "Witch Bolt"],
   warlock: ["Armor of Agathys", "Arms of Hadar", "Charm Person", "Comprehend Languages", "Expeditious Retreat", "Hellish Rebuke", "Hex", "Illusory Script", "Protection from Evil and Good", "Unseen Servant", "Witch Bolt"],
   bard: ["Bane", "Charm Person", "Comprehend Languages", "Cure Wounds", "Detect Magic", "Disguise Self", "Faerie Fire", "Feather Fall", "Healing Word", "Heroism", "Identify", "Illusory Script", "Silent Image", "Sleep", "Speak with Animals", "Tasha's Hideous Laughter", "Thunderwave", "Unseen Servant"],
-  paladin: ["Bless", "Command", "Compelled Duel", "Cure Wounds", "Detect Evil and Good", "Detect Magic", "Detect Poison and Disease", "Divine Favor", "Heroism", "Protection from Evil and Good", "Purify Food and Drink", "Searing Smite", "Shield of Faith", "Thunderous Smite", "Wrathful Smite"]
+  paladin: ["Bless", "Command", "Compelled Duel", "Cure Wounds", "Detect Evil and Good", "Detect Magic", "Detect Poison and Disease", "Divine Favor", "Heroism", "Protection from Evil and Good", "Purify Food and Drink", "Searing Smite", "Shield of Faith", "Thunderous Smite", "Wrathful Smite"],
+  fey_touched: ["Bane", "Bless", "Charm Person", "Command", "Compelled Duel", "Comprehend Languages", "Detect Magic", "Detect Poison and Disease", "Disguise Self", "Heroism", "Identify", "Sleep", "Speak with Animals", "Tasha's Hideous Laughter"],
+  shadow_touched: ["Bane", "Cause Fear", "Charm Person", "Disguise Self", "False Life", "Inflict Wounds", "Ray of Sickness", "Silent Image"]
 };
-// Special spell lists for feats
-const SPELL_LISTS = {
-  fey_touched: ["Misty Step", "Bane", "Bless", "Charm Person", "Command", "Compelled Duel", "Comprehend Languages", "Detect Magic", "Detect Poison and Disease", "Disguise Self", "Heroism", "Identify", "Sleep", "Speak with Animals", "Tasha's Hideous Laughter"],
-  shadow_touched: ["Invisibility", "Bane", "Cause Fear", "Charm Person", "Disguise Self", "False Life", "Inflict Wounds", "Ray of Sickness", "Silent Image"]
-};
+
 // Spell sources tracking - tracks which features grant spells
 const SPELL_SOURCES = {
   // Origin Feats
@@ -42,6 +40,7 @@ const SPELL_SOURCES = {
   "Spell Lore": { cantrips: 1, cantripList: "bard", level1: 1, level1List: "bard" },
   "Wild Charm": { cantrips: 0, cantripList: null, level1: 1, level1List: "druid" },
   "Battle Hymn": { cantrips: 1, cantripList: "bard", level1: 0, level1List: null },
+  "Hunter's Mark": { cantrips: 0, cantripList: null, level1: 0, level1List: null, fixedSpells: ["Hunter's Mark"], note: "Always prepared; can cast 1/day without a slot or by using slots" },
   
   // General Feats
   "Fey Touched": { 
